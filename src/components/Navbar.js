0c7ede641f849dabf8c1f93372df4924c5749b8f
vicky <vigmurug@gmail.com>
2023-02-27 23:06:42 -0500

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-      <Link to="/" className="navbar">
+      <Link to="/coincatalogue" className="navbar">
           <FaCoins className="icon" />
           <h1>Coin <span className="site-name">Catalogue</span></h1>
       </Link>
